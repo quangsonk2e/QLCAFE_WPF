d09694c6f7ee00c602e0e75fe82fab21263eccc6
mrquangson <quangsonk2e@gmaill.com>
2020-07-08 22:32:02 +0700

    đ

---
+++ QLCAFE_WPF/Viewmodel/MainVM.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace QLCAFE_WPF.Viewmodel
+{
+    public class MainVM:BaseViewModel
+    {
+        public ICommand them { get; set; }
+        public MainVM()
+        {
+            them = new RelayCommand<Object>(x=>true,x=>{
+                var tabs = (DXTabControl)x;
+                DXTabItem a = new DXTabItem();
+                a.Content = new UserCT.UserControl1();
+                tabs.Items.Add(a);
+            
+            }
+                
+                );
+        }
+    }
+}
